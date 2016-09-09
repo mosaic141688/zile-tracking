a1cae1609cd181dbcdbf80074e205287d3f28474
mosaic141688 <mbhamalimosaic@google.com>
2016-09-09 07:29:31 +0200

    bs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express();
-app.use(express.static('public'));
+app.use(express.static('static'));
 
 app.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
