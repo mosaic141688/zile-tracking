1a1ec7e56278aa577d032fa8965b57a45485756a
mosaic141688 <mbhamalimosaic@google.com>
2016-09-10 10:05:22 +0200

    socket

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var express = require('express')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 app.use(express.static('static'));
+
+var routes = [];
+
+io.on("connecton",function(socket){
+  console.log("Device Connected");
+})
 
 app.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
