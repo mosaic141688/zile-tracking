12040cc2fa081c00139d11b1ffe73924642e56b4
mosaic141688 <mbhamalimosaic@google.com>
2016-09-11 07:56:38 +0200

    DB

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ var express = require('express')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var db = require('./db');
 app.use(express.static('static'));
 
 var routes = [];
 
 io.on("connection",function(socket){
   console.log("Device Connected");
-  socket.on("coords",(cd)=>console.log(cd));
+  socket.on("coords",function(cd){
+    db.save(cd,(es)=>console.log(es));
+  });
+})
+
+app.get("locs",function(req,res) {
+  db.getLocations((dat)=>res.send(dat));
 })
 
 http.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || 'localhost',function () {
---
+++ db.js
@@ -0,0 +1,56 @@
+var mongoose = require('mongoose');
+
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+   url = process.env.OPENSHIFT_MONGODB_DB_URL +
+       process.env.OPENSHIFT_APP_NAME;
+}
+
+// Connect to mongodb
+var connect = function () {
+   mongoose.connect(url);
+};
+connect();
+
+var db = mongoose.connection;
+
+db.on('error', function(error){
+   console.log("Error loading the db - "+ error);
+});
+
+db.on('disconnected', connect);
+
+var LocationSchema = new mongoose.Schema({
+   device:String,
+   lat:Number,
+   lng:Number
+})
+
+
+var Location = mongoose.model("user",LocationSchema);
+
+var save=function(loc,callback){
+  Location.create(loc,(err,res)=>err?console.log(err):callback(res));
+}
+exports.save=save;
+var getDevices=function(callback){
+  Location.aggregate([
+        {
+            $group: {
+                _id: '$device',  //$region is the column name in collection
+                count: {$sum: 1}
+            }
+        }
+    ], function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            callback(result);
+        }
+    });
+}
+exports.getDevices=getDevices;
+var getLocations = function(callback){
+  Location.find({},'lat,lng',(err,res)=>err?console.log(err):callback(res));
+}
+exports.getLocations=getLocations;
