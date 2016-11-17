4f5ea888bcb6394fb8ee0c29ea00d36507c9d724
mosaic141688 <mbhamalimosaic@gmail.com>
2016-11-17 20:41:43 +0200

    Update

---
+++ app (copy).js
@@ -0,0 +1,77 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var db = require('./db');
+app.use(express.static('ui'));
+
+var routes = [];
+
+io.on("connection",function(socket){
+  console.log("Device Connected");
+  socket.on("dev-status",(cd)=>{
+    db.setDeviceOnline(cd,()=>console.log("Device ${cd} Went offlne"))
+      console.log("Reg Query:"+cd);
+    db.deviceRegistered(cd,(reg)=>{
+    console.log("Reg Query:"+cd);
+    socket.emit("registered",reg.registered)
+  })
+  socket.on("disconnect",()=>{
+    console.log("Device offline");
+    db.setDeviceOffline(cd,()=>console.log('device'+cd+'went offline'));
+  })
+})
+  socket.on("coords",function(cd){
+    db.updateDevice(cd,(es)=>console.log(es));
+  //  db.deviceRegistered(cd,(reg)=>socket.emit("registered",reg));
+  });
+  socket.on("signup",(_school)=>{
+    db.registerSchool(_school,(res)=>{
+      socket.emit("signed",res);
+    });
+  });
+
+  socket.on("get-device",(_dev_id)=>{
+    db.getDevice(_dev_id,(res)=>{
+      socket.emit("got-device",res);
+    })
+  })
+
+
+  socket.on("get-school",(_school_id)=>{
+    console.log(_school_id);
+  });
+
+  socket.on("reg-device",(params)=>{
+    console.log(JSON.stringify(params));
+    db.registerDevice(params.dev,params.school._id,(res)=>console.log(res))
+  })
+})
+app.get("/alldevices",(req,res)=>db.allDevices((dat)=>res.send(dat)));
+app.post("/devices",(req,res)=>{
+  console.log(req.body);
+});
+app.post("/school",(req,resp)=>db.getSchools({_id:db._ID(req.body._id)},(res)=>{
+  resp.send(res)
+}));
+app.post("/schools",(req,resp)=>db.getSchools({},(res)=>resp.send(res)));
+app.post("/reportlost",(req,resp)=>db.setLost(req.body.dev_id,(res)=>resp.send(res)));
+app.post("/recover",(req,resp)=>db.recover(req.body.dev_id,(res)=>resp.send(res)));
+app.post("/signup",(req,resp)=>db.registerSchool(req.body._school,(res)=>resp.send(res)));
+app.post("/login",(req,resp)=>db.signinSchool(req.body.email,req.body.password,(res)=>resp.send(res)));
+app.post("/device",(req,resp)=>db.getDevice(req.body.dev_id,(res)=>resp.send(res)));
+app.post("/regdevice",(req,resp)=>db.registerDevice(req.body.dev_id,req.body.school_id,(res)=>resp.send(res)));
+//app.post("/lostDevices",(req,resp)=>db.)
+app.post("/locs",function(req,res) {
+  db.getLocations((dat)=>res.send(dat));
+})
+app.post("/removeDevice",(req,resp)=>db.removeDevice(req.body.dev_id,req.body.school,(res)=>resp.send(res)));
+app.get("/locs",function(req,res) {
+  db.getAllLocations((dat)=>res.send(dat));
+})
+http.listen(process.env.NODE_PORT || 3000, process.env.NODE_IP || "0.0.0.0",function () {
+  console.log(`Application worker ${process.pid} started...`);
+});
---
+++ db (copy).js
@@ -0,0 +1,211 @@
+
+var mongoose = require('mongoose');
+var url="mongodb://localhost/zile:27017";
+// if OPENSHIFT env variables are present, use the available connection info:
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+   url = process.env.OPENSHIFT_MONGODB_DB_URL +
+       process.env.OPENSHIFT_APP_NAME;
+}
+
+// Connect to mongodb
+var connect = function () {
+   mongoose.connect(url);
+};
+
+connect();
+exports._ID = mongoose.Types.ObjectId;
+var db = mongoose.connection;
+
+db.on('error', function(error){
+   console.log("Error loading the db - "+ error);
+});
+
+db.on('disconnected', connect);
+
+var LocationSchema = new mongoose.Schema({
+   device:String,
+   lat:Number,
+   lng:Number
+})
+
+var SchoolSchema = new mongoose.Schema({
+   name:String,
+   email:String,
+   address:String,
+   tel:String,
+   province:String,
+   password:String,
+   devices:[{type:String,ref:"device"}]
+})
+
+
+
+
+
+var DeviceSchema = mongoose.Schema({
+  _id:String,
+  status:{type:String,default:"offline"},
+  lost:{type:Boolean,default:false},
+  registered:{type:Boolean, default:false},
+  location:{lat:Number,lng:Number},
+  trail:[{lat:Number,lng:Number,time:{type:Date,default:Date.now()}}]
+});
+
+
+
+var Device = mongoose.model("device",DeviceSchema);
+
+var School = mongoose.model("school",SchoolSchema);
+exports.setDeviceOnline=function(dev_id,callback){
+  Device.update({_id:dev_id},{status:"online"},(err,res)=>err?console.log(err):callback(res));
+}
+exports.setDeviceOffline = function(dev_id,callback){
+  Device.update({_id:dev_id},{status:"offline"},(err,res)=>err?console.log(err):callback(res))
+}
+
+exports.deviceRegistered = function(dev,callback){
+  Device.findOne({_id:dev},(err,res)=>err?console.log(err):callback(res));
+}
+
+var signinSchool = function(email,password,callback){
+  School.findOne({email:email, password:password},(err,res)=>err?console.log(err):callback(res));
+}
+
+exports.signinSchool=signinSchool;
+var registerDevice = function(dev,sch,callback){
+  Device.findOne({_id:dev},(err,res)=>{
+    if(err){
+      console.log(err);
+    }
+else if (res) {
+  if(res.registered){
+    callback("1111");
+    return;
+  }
+  School.update({_id:mongoose.Types.ObjectId(sch)},{$push:{devices:dev}},(er,re)=>er?console.log(er):callback(re));
+  res.registered=true;
+  res.save();
+}
+else{
+  callback("0000");
+}
+  })
+}
+
+exports.registerDevice=registerDevice;
+
+var getSchools = function(callback){
+  School.find({},(err,res)=>err?console.log(err):callback(res));
+}
+
+var getDeviceIds = function(callback){
+  Device.find({},'_id',(err,res)=>err?console.log(err):callback(res));
+}
+
+
+var getSchool = function(sch, callback){
+  School.findOne({_id:sch}).populate("devices").exec((err,res)=>err?console.log(err):callback(res));
+}
+
+var registerSchool=function(sch,callback){
+  School.findOne({email:sch.email},function(err,res){
+    if(err){
+      console.log(err);
+    }
+    else {
+      if (res) {
+        callback(res)
+      }
+      else{
+        School.create(sch,(er,re)=>er?console.log(er):callback(re));
+      }
+    }
+  })
+}
+
+exports.registerSchool = registerSchool;
+
+var updateDevice = function(dev,callback){
+  Device.findOne({_id:dev.device},function(err,res){
+    if(err)
+    console.log(err);
+    else {
+      if(res){
+        console.log("Device Exists");
+        Device.update({_id:dev.device},{'location.lat':dev.lat,'location.lng':dev.lng,$push:{trail:{lat:dev.lat,lng:dev.lng}}},(er,re)=>er?console.log(er):console.log(re));
+      }
+      else{
+        Device.create({_id:dev.device,location:{lat:dev.lat,lng:dev.lng}});
+      }
+    }
+  })
+}
+
+var allDevices=function(callback){
+  Device.find({},(err,res)=>err?console.log(err):callback(res));
+}
+
+var schools = function(query,population,callback){
+  School.find(query).populate(population).exec((err,res)=>err?console.log(err):callback(res));
+}
+
+exports.schools=schools;
+
+exports.allDevices=allDevices;
+
+exports.updateDevice=updateDevice;
+var Location = mongoose.model("user",LocationSchema);
+var save=function(loc,callback){
+  Location.create(loc,(err,res)=>err?console.log(err):callback(res));
+}
+exports.save=save;
+var getDevices=function(callback){
+  Location.aggregate([
+        {
+            $group: {
+                _id: '$device',  //$region is the column name in collection
+                count: {$sum: 1}
+            }
+        }
+    ], function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            callback(result);
+        }
+    });
+}
+exports.getDevices=getDevices;
+var getLocations = function(callback){
+  Location.find({},'lat lng',(err,res)=>err?console.log(err):callback(res));
+}
+
+exports.getDevice = function(_dev_id,callback){
+    Device.find({_id:_dev_id},(err,res)=>err?console.log(err):callback(res));
+}
+
+exports.removeDevice=function(_dev_id,_sch,callback){
+    Device.update({_id:_dev_id},{registered:false});
+  School.update({_id:mongoose.Types.ObjectId(_sch)},{$pull:{devices:_dev_id}},(err,res)=>err?console.log(err):callback(res));
+}
+
+exports.resetRegistration=function (_dev_id,callback) {
+    Device.update({_id:_dev_id},{registered:false},(err,res)=>err?console.log(err):callback(res));
+}
+
+exports.setLost=function(_dev_id,callback){
+  Device.update({_id:_dev_id},{lost:true},(err,res)=>err?console.log(err):callback(res));
+}
+
+exports.recover = function(_dev_id,callback){
+  Device.update({_id:_dev_id},{lost:false},(err,res)=>err?console.log(err):callback(res));
+}
+
+var getAllLocations = function(callback){
+  Location.find({},(err,res)=>err?console.log(err):callback(res));
+}
+exports.getSchools=function(query,callack){
+  School.find(query).populate('devices').exec((err,res)=>err?console.log(err):callack(res));
+}
+exports.getAllLocations=getAllLocations;
+exports.getLocations=getLocations;
